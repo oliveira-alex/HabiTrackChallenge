c688fe520c01a90ed31cfb5fe8291cd567f8cabb
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-20 13:26:01 -0300

    Initial Commit

---
+++ HabiTrackChallenge/HabiTrackChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabiTrackChallengeApp.swift
+//  HabiTrackChallenge
+//
+//  Created by Alex Oliveira on 20/09/21.
+//
+
+import SwiftUI
+
+@main
+struct HabiTrackChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
