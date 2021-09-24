1f7cf73959dab781ce5023893e16e5f53345c6f1
ALeX <alexdeoliveira.poli@gmail.com>
2021-09-24 19:15:11 -0300

    Sheet presentation now fully implemented

--- HabiTrackChallenge/AddHabitView.swift
+++ HabiTrackChallenge/AddHabitView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct AddHabitView: View {
+    @Environment(\.presentationMode) var presentationMode
     @ObservedObject var habits: Habits
     
+    @State private var newHabitName = ""
+    @State private var newHabitDescription = ""
+    
+    @State private var showingError = false
+    
+    func addNewHabit() {
+//        if !habits.contains(newHabit) {
+//            habits.append(newHabit)
+//
+//            newHabit = ""
+//        } else {
+//            showingError.toggle()
+//    }
+
+        let newHabit = Habit(name: newHabitName, description: newHabitDescription, timesPracticed: 1)
+        habits.add(newHabit)
+
+        newHabitName = ""
+    }
+
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Form {
+                TextField("New habit name", text: $newHabitName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                TextField("New habit description (optional)", text: $newHabitDescription)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+            }
+            .navigationBarTitle("Add New Habit")
+            .navigationBarItems(
+                leading: Button("Cancel") { self.presentationMode.wrappedValue.dismiss() },
+                trailing: Button("Save") {
+                    if newHabitName == "" { newHabitName = "New Habit" }
+                    
+                    let newHabit = Habit(name: newHabitName, description: newHabitDescription, timesPracticed: 1)
+                    self.habits.add(newHabit)
+                    
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+             )
+        }
     }
 }
 
--- HabiTrackChallenge/ContentView.swift
+++ HabiTrackChallenge/ContentView.swift
@@ -8,9 +8,10 @@
 import SwiftUI
 
 struct Habit: Identifiable {
-    var name: String = "New Habit"
-    var timesPracticed: Int = 1
-    let id = UUID()
+    var name: String
+    var description: String
+    var timesPracticed: Int
+    var id = UUID()
 }
 
 class Habits: ObservableObject {
@@ -18,9 +19,9 @@ class Habits: ObservableObject {
     
     init() {
         self.items = [
-            Habit(name: "Coding", timesPracticed: 365),
-            Habit(name: "Running", timesPracticed: 9),
-            Habit(name: "Lifting", timesPracticed: 80)
+            Habit(name: "Coding", description: "Hacking with Swift", timesPracticed: 365),
+            Habit(name: "Running", description: "Flying low", timesPracticed: 9),
+            Habit(name: "Lifting", description: "Sweating my heart out", timesPracticed: 80)
         ]
     }
     
@@ -45,74 +46,51 @@ class Habits: ObservableObject {
 }
 
 struct ContentView: View {
-    @State private var newHabitName = ""
     @ObservedObject var habits = Habits()
 
-    @State private var showingError = false
     @State private var showingAddHabitSheet = false
     
     var body: some View {
         NavigationView {
-//            VStack {
-//                TextField("Enter a new habit", text: $newHabitName, onCommit: addNewHabit)
-//                    .textFieldStyle(RoundedBorderTextFieldStyle())
-//                    .padding([.top, .horizontal])
-//
-                List {
-                    ForEach(habits.items) { habit in
-                        HStack {
-                            VStack(alignment: .leading) {
-                                Text(habit.name)
-                                    .font(.headline)
-                                HStack {
-                                    Text(String(habit.timesPracticed))
-                                    Text(habit.timesPracticed >= 2 ? "Times" : "Time")
-                                        .font(.subheadline)
-                                }
+            List {
+                ForEach(habits.items) { habit in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(habit.name)
+                                .font(.headline)
+                            HStack {
+                                Text(String(habit.timesPracticed))
+                                Text(habit.timesPracticed >= 2 ? "Times" : "Time")
+                                    .font(.subheadline)
                             }
-                            
-                            Stepper("",
-                                    onIncrement: { habits.incrementTimesPracticedOf(habit) },
-                                    onDecrement: { habit.timesPracticed > 0 ? habits.decrementTimesPracticedOf(habit) : nil }
-                            )
                         }
+                        
+                        Stepper("",
+                                onIncrement: { habits.incrementTimesPracticedOf(habit) },
+                                onDecrement: { habit.timesPracticed > 0 ? habits.decrementTimesPracticedOf(habit) : nil }
+                        )
                     }
                 }
-//            }
+            }
             .navigationBarTitle("Habits")
-                .navigationBarItems(
-                    leading: EditButton(),
-                    trailing: Button(action: {
-                        self.showingAddHabitSheet.toggle()
-                    }) {
-                        Image(systemName: "plus")
-                    }
-                )
-                .sheet(isPresented: $showingAddHabitSheet) {
-//                    print("showing add sheet")
-                    AddHabitView(habits: self.habits)
+            .navigationBarItems(
+                leading: EditButton(),
+                trailing: Button(action: {
+                    self.showingAddHabitSheet.toggle()
+                }) {
+                    Image(systemName: "plus")
                 }
+            )
+            .sheet(isPresented: $showingAddHabitSheet) {
+//                    print("showing add sheet")
+                AddHabitView(habits: self.habits)
+            }
 //            .alert(isPresented: $showingError) {
 //                Alert(title: Text("Already added"),
 //                      message: Text("Your list already has a habit with this same name"),
 //                      dismissButton: .default(Text("OK")))
 //            }
         }
-    }
-    
-    func addNewHabit() {
-//        if !habits.contains(newHabit) {
-//            habits.append(newHabit)
-//
-//            newHabit = ""
-//        } else {
-//            showingError.toggle()
-//    }
-
-        let newHabit = newHabitName != "" ? Habit(name: newHabitName, timesPracticed: 1) : Habit()
-        habits.add(newHabit)
-
-        newHabitName = ""
     }
 }
 
